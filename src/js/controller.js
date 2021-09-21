c50d534302910cf9cf34a8678a0ac8a3f578451d
kuzeva <kuzeva01@gmail.com>
2021-09-21 14:39:39 +0200

    implemented update algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -15,10 +15,14 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0. Update results view to mark selected search result
+    recipeView.update(model.getSearchResultsPage());
+
     // 1. Loading recipe
     await model.loadRecipe(id);
 
     // 2. Rendering the recipe
+    console.log(model.state.recipe);
     recipeView.render(model.state.recipe);
   } catch (err) {
     console.log(err);
@@ -57,7 +61,8 @@ const controlServings = function (newServings) {
   //Update the recipe servings(in state)
   model.updateServings(newServings);
   //Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -11,11 +11,14 @@ class ResultsView extends View {
     return this._data.map(this._generateMarkupPreview).join('');
   }
   _generateMarkupPreview(result) {
+    const id = window.location.hash.slice(1);
     return `
       <li class="preview">
-         <a class="preview__link" href="#${result.id}">
+         <a class="preview__link ${
+           result.id === id ? 'preview__link--active' : ''
+         }" href="#${result.id}">
             <figure class="preview__fig">
-               <img src="${result.image}" alt="food image" />
+               <img src=${result.image} alt="food image" />
             </figure>
             <div class="preview__data">
                <h4 class="preview__title">${result.title}</h4>
